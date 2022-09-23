b1224e337a517ebb3183a59b3b0b1d7fefab3508
quang1409thanh <thanhaxt342@gmail.com>
2022-09-24 02:27:56 +0700

    game loop dùng các phím oke nhưng chưa được tốt, gameloop denlta hay tích lỹ

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,12 +18,24 @@ public class GamePanel extends JPanel implements Runnable{
 	final int screenWidth = titleSize* MaxScreenCol;//768
 	final int screenHeight = titleSize * MaxScreenRow ; //576
 	
+	// FPS
+	int FPS = 60;
+	
+	KeyHandlee keyH = new KeyHandlee();
 	Thread gameThread;
+	
+	// set player's default position
+	
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
 	public GamePanel() {
 	
 		this.setPreferredSize(new Dimension(screenWidth,screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	
 	public void startGameThread() {
@@ -34,19 +46,48 @@ public class GamePanel extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+		
+		double drawInterval = 1000000000/FPS; // O.0166 second
+		double nextDrawTime =  System.nanoTime() + drawInterval;
 		while(gameThread!=null) {
-			//System.out.println("The game loop is running");
-			// 1 update: update information such as character positions
+			
 			update();
 			
-			// 1 draw: draw the screen width the updated information
 			repaint();
+			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				
+				remainingTime = remainingTime/1000000;
+				
+				if (remainingTime < 0) {
+					remainingTime = 0;
+				}
+				Thread.sleep((long) remainingTime );
+				
+				nextDrawTime += drawInterval;
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
 		}
 		
 	}
 	
 	public void update() {
 		
+		if(keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		else if(keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if(keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -57,7 +98,7 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		g2.setColor(Color.white);
 		
-		g2.fillRect(100, 100,titleSize, titleSize);
+		g2.fillRect(playerX	, playerY,titleSize, titleSize);
 		
 		g2.dispose();
 	}
